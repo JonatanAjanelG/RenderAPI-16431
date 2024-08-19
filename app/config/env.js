1b3c2ef27f0e30e72b9980a0294dacb4b74847f2
JonatanAjanelG <136577847+JonatanAjanelG@users.noreply.github.com>
2024-08-18 22:04:55 -0600

    Primer commit al repositorio

---
+++ app/config/env.js
@@ -0,0 +1,18 @@
+
+
+const env = {
+  database: 'umg_salama_16431',
+    username: 'umg_salama_16431_user',
+    password: 'bf7OzTK7F7GZt1S5DpsOPlMqSTCxwuVF',
+    host: 'dpg-cqin55eehbks73c0p4s0-a.oregon-postgres.render.com', 
+    //Aqui se debe de modificar el host de externo para interno
+    dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+module.exports = env; 
