e6caf0b002c43ef849ab43bc9df66c62eb48861d
JonatanAjanelG <136577847+JonatanAjanelG@users.noreply.github.com>
2024-08-24 17:20:15 -0600

    na

--- app/config/env.js
+++ app/config/env.js
@@ -1,12 +1,10 @@
 
 
 const env = {
-  database: 'umg_salama_16431',
+    database: 'umg_salama_16431',
     username: 'umg_salama_16431_user',
     password: 'dpg-cqin55eehbks73c0p4s0-a.oregon-postgres.render.com',
-    host: '', 
-    
-    //Aqui se debe de modificar el host de externo para interno
+    host: 'dpg-cqin55eehbks73c0p4s0-a', 
     dialect: 'postgres',
   pool: {
     max: 5,
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const db = require('./app/config/db.config.js');
   
 // force: true will drop the table if it already exists
 db.sequelize.sync({force: true}).then(() => {
-  console.log('Drop and Resync with { force: false }');
+  console.log('Drop and Resync with { force: true }');
 }); 
 
 let router = require('./app/routers/router.js');
